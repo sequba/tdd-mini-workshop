00595d1a295ae981946e5a2e5b5ea4daf26135f2
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2023-12-14 13:48:19 +0100

    Update task 1 description

--- task1/divideNumber.spec.ts
+++ task1/divideNumber.spec.ts
@@ -2,11 +2,11 @@ import { describe } from 'vitest'
 
 /*
 Write a function that divides 2 numbers.
-- function should accept numbers or strings
-- in case of string it should convert it to a number first
-- handle all corner cases and special situations
+Handle all corner cases and special situations.
+
+New requirements will arrive later.
 */
 
-describe('', () => {
+describe('divideNumber', () => {
 
 });
